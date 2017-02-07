fdfe6f360a3b33555cc44ef3d6c81b2830763454
Sara Palomares <sarapalomares@me.com>
2017-02-06 20:32:11 -0800

    Initial Commit

---
+++ rubyPuzzles.rb
@@ -0,0 +1,18 @@
+# Create an array with the following values: 3,5,1,2,7,9,8,13,25,32. Print the sum of all numbers in the array. Also have the function return an array that only include numbers that are greater than 10 (E.g. when you pass the array above, it should return an array with the values of 13,25,32 - hint: use reject or find_all method).
+x = [3,5,1,2,7,9,8,13,25,32]
+
+# Create an array with the following values: John, KB, Oliver, Cory, Matthew, Christopher. Shuffle the array and print the name of each person. Have the program also return an array with names that are longer than 5 characters.
+
+
+# Create an array that contains all 26 letters in the alphabet (this array must have 26 values). Shuffle the array and display the last letter of the array. Have it also display the first letter of the array. If the first letter in the array is a vowel, have it display a message.
+
+
+# Generate an array with 10 random numbers between 55-100.
+
+# Generate an array with 10 random numbers between 55-100 and have it be sorted (showing the smallest number in the beginning). Display all the numbers in the arrays. Next, display the minimum value in the array as well as the maximum value.
+
+
+# Create a random string that is 5 characters long (hint: (65+rand(26)).chr returns a random character).
+
+
+# Generate an array with 10 random strings that are each 5 characters long.
