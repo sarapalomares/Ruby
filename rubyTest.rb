4fe3de58eab29962063c3cedad45dc7c2c6ae7b8
Sara Palomares <sarapalomares@me.com>
2017-02-08 10:45:35 -0800

    Used to test

--- rubyTest.rb
+++ rubyTest.rb
@@ -1,2 +1,25 @@
-avg = [2, 10, 3]
-puts avg.reduce(:+) / avg.size
+class CodingDojo
+      @@no_of_branches = 0 #class variable. always begins with @@
+      def initialize(id, name, address) #local variable. begins with a lowercase or _
+            @branch_id = id #instance variable. always begins with @
+            @branch_name = name
+            @branch_address = address
+            @@no_of_branches += 1
+            puts "\nCreated branch #{@@no_of_branches}" #\n creayes a new line
+      end
+
+      def hello
+            puts "Hello CodingDojo!"
+      end
+
+      def displayAll
+            puts "Branch ID: %d" % @branch_id
+            puts "Branch Name: %s" % @branch_name
+            puts "Branch Address: %s" % @branch_address
+      end
+end
+# now using above class to create objects
+branch = CodingDojo.new(253, "SF CodingDojo", "Sunnyvale CA")
+branch.displayAll
+branch2 = CodingDojo.new(155, "Boston CodingDojo", "Boston MA")
+branch2.displayAll
