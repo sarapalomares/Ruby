908244b50fa800669ce69c8c5aa5566716fc562c
Sara Palomares <sarapalomares@me.com>
2017-02-09 10:03:37 -0800

    Revised

--- Lectures/lecture1-fundamentals.rb
+++ Lectures/lecture1-fundamentals.rb
@@ -1,39 +1,39 @@
 arr = [22,33,44,55,66]
 
-# for i in 0...arr.count
-#       puts "Our current number is: #{arr[i]}, and our index is ${i}"
-# end
+for i in 0...arr.count
+      puts "Our current number is: #{arr[i]}, and our index is ${i}"
+end
 
 # -----------------------------------------------------------------------------------------------------
 
-# sum = 0
-# arr.each do |num|
-#       puts "The sum is #{sum}"
-#       sum += num
-#       puts "The sum is now #{sum} after adding #{num}"
-# end
+sum = 0
+arr.each do |num|
+      puts "The sum is #{sum}"
+      sum += num
+      puts "The sum is now #{sum} after adding #{num}"
+end
 
 # -----------------------------------------------------------------------------------------------------
 
-# obj = {first_name: "John", last_name: "Doe", occupation: "Instructor"}
-# obj.each do |key, value|
-#       puts "The key is: #{key}, and the value is #{value}."
-# end
+obj = {first_name: "John", last_name: "Doe", occupation: "Instructor"}
+obj.each do |key, value|
+      puts "The key is: #{key}, and the value is #{value}."
+end
 
 # -----------------------------------------------------------------------------------------------------
 
-# person = {:first_name => "John", :last_name => "Doe", :occupation => "Instructor"}
-#       puts person[:occupation]
+person = {:first_name => "John", :last_name => "Doe", :occupation => "Instructor"}
+      puts person[:occupation]
 
 # -----------------------------------------------------------------------------------------------------
-#
-# def dojo
-#       puts "Just started the function"
-#       yield 55
-#       puts "Just left the function"
-# end
-#
-# dojo {|num| puts "The number you gave the yield is #{num}"}
+
+def dojo
+      puts "Just started the function"
+      yield 55
+      puts "Just left the function"
+end
+
+dojo {|num| puts "The number you gave the yield is #{num}"}
 
 # -----------------------------------------------------------------------------------------------------
 
